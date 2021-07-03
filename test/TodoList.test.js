f37bc5d9bc19608ad28c96219c4fcc19d38347bb
Andrew cooke <andrewcooke@Andrews-MacBook-Pro.local>
2021-07-03 12:50:08 +0100

    Test: Smart contract taskCompleted implemented and tested

--- test/TodoList.test.js
+++ test/TodoList.test.js
@@ -22,7 +22,7 @@ contract('TodoList', accounts => {
     assert.equal(task.content, 'First Todo')
     assert.equal(task.completed, false)
     assert.equal(taskCount.toNumber(), 1)
-  })
+  });
 
   it('Creates tasks', async () => {
     const result = await this.todoList.createTask('A new task')
@@ -32,6 +32,15 @@ contract('TodoList', accounts => {
     assert.equal(event.id.toNumber(), 2)
     assert.equal(event.content, 'A new task')
     assert.equal(event.completed, false)
-  })
+  });
 
+
+  it('toggles task completion', async () => {
+    const result = await this.todoList.toggleCompleted(1)
+    const task = await this.todoList.tasks(1);
+    assert.equal(task.completed, true);
+    const event = result.logs[0].args;
+    assert.equal(event.id.toNumber(), 1)
+    assert.equal(event.completed, true)
+  });
 })
