271127c32273d589a1dbbdb94e508d7e919d2c4b
Andrew cooke <andrewcooke@Andrews-MacBook-Pro.local>
2021-07-03 12:27:43 +0100

    Test: added smart contract create event with tests

--- test/TodoList.test.js
+++ test/TodoList.test.js
@@ -1,3 +1,4 @@
+const { assert } = require("chai");
 
 const TodoList = artifacts.require('./TodoList.sol');
 
@@ -22,4 +23,15 @@ contract('TodoList', accounts => {
     assert.equal(task.completed, false)
     assert.equal(taskCount.toNumber(), 1)
   })
+
+  it('Creates tasks', async () => {
+    const result = await this.todoList.createTask('A new task')
+    const taskCount = await this.todoList.taskCount();
+    assert.equal(taskCount, 2);
+    const event = result.logs[0].args;
+    assert.equal(event.id.toNumber(), 2)
+    assert.equal(event.content, 'A new task')
+    assert.equal(event.completed, false)
+  })
+
 })
